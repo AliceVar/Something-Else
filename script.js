946d1b142ef522f53d93f3fed9233021214c74d2
Alice <varshavska.alina@gmail.com>
2026-06-07 00:25:03 -0600

    color change,section added, pics updated

--- script.js
+++ script.js
@@ -19,3 +19,42 @@ tabButtons.forEach((button) => {
     selectedMenu.classList.add("active");
   });
 });
+
+const reveals = document.querySelectorAll(".reveal");
+
+function revealOnScroll() {
+  reveals.forEach((item) => {
+    const windowHeight = window.innerHeight;
+    const elementTop = item.getBoundingClientRect().top;
+
+    if (elementTop < windowHeight - 100) {
+      item.classList.add("active");
+    }
+  });
+}
+
+window.addEventListener("scroll", revealOnScroll);
+
+revealOnScroll();
+
+
+const modal = document.getElementById("imageModal");
+const modalImg = document.getElementById("modalImg");
+
+document.querySelectorAll(".story-gallery img, .slider-track img")
+  .forEach((img) => {
+    img.addEventListener("click", () => {
+      modal.style.display = "flex";
+      modalImg.src = img.src;
+    });
+  });
+
+document.querySelector(".close").addEventListener("click", () => {
+  modal.style.display = "none";
+});
+
+modal.addEventListener("click", (e) => {
+  if (e.target === modal) {
+    modal.style.display = "none";
+  }
+});
